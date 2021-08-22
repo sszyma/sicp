7ce7e175de756db5f2e707a516be0ed1668a6a41
Stanislaw Szyma <szyma.stanislaw@gmail.com>
2021-08-22 19:25:29 +0200

    I'm back

---
+++ ch1/11.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+
+;; A direct, recursive implementation of the function f
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
+
+;; An iterative implementation
+(define (f2 n)
+  (define (iter a b c count)
+    (if (= count 0)
+        c
+        (iter (+ a (* 2 b) (* 3 c))
+              a
+              b
+              (- count 1))))
+  (iter 2 1 0 n))
---
+++ ch1/12.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+
+;; computes the binomial coefficient for 0 <= k <= n
+(define (binomial n k)
+  (cond ((= n 0) 1)
+        ((or (= n k) (= k 0)) 1)
+        (else (+ (binomial (- n 1) k)
+                 (binomial (- n 1) (- k 1))))))
